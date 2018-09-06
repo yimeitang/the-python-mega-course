e2472de9e0887b03efcb5a7b39551c256f5de496
Yimei Tang <yimeitang1230@gmail.com>
2018-09-05 20:17:21 -0500

    Update script1.py

--- section10/Application4/script1.py
+++ section10/Application4/script1.py
@@ -6,5 +6,12 @@ app=Flask(__name__)
 def home():                                      #define what the homepage will do
     return "Website content goes here!"
 
+@app.route('/anotherpage/')
+def about():
+    return "This is another page"
+    
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True)                          #run home() function
+
+    
+    
