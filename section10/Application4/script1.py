7665fcea1f2ae7c35df39402a210538bdf9ff85f
Yimei Tang <yimeitang1230@gmail.com>
2018-09-05 20:13:34 -0500

    Update script1.py

--- section10/Application4/script1.py
+++ section10/Application4/script1.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app=Flask(__name__)
 
-@app.route('/')                                  #the decorator
+@app.route('/')                                  #the decorator, the content will be mapped here, you could also change this to @app.rout('/about/')
 def home():                                      #define what the homepage will do
     return "Website content goes here!"
 
